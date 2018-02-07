cb05fe19bd3bdc827e754bf761739befb004bcc0
Deepal Dsilva <dsilvadeepal@gmail.com>
2018-02-07 15:44:11 -0500

    Decision trees for Kaggle Titanic problem

--- Kaggle/Titanic/RScript_Titanic.R
+++ Kaggle/Titanic/RScript_Titanic.R
@@ -20,3 +20,31 @@ test$Survived[test$Sex == 'female'] <- 1
 
 submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
 write.csv(submit, file = "onlyfemalessurvive.csv", row.names = FALSE)
+
+aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+train$Fare2 <- '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+train$Fare2[train$Fare < 10] <- '<10'
+table(train$Fare2)
+
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+#Import rpart i.e. Recursive Partitioning and Regression Trees
+#Using Decision Trees
+library(rpart)
+fit <- rpart(Survived ~ Pclass + Sex + Age +  SibSp + Parch + Fare + Embarked, data=train, method="class")
+
+library(rpart.plot)
+library(RColorBrewer)
+library(rattle)
+fancyRpartPlot(fit)
+
+Prediction <- predict(fit, test, type = "class")
+submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
+write.csv(submit, file = "myfirstdtree.csv", row.names = FALSE)
+
+#Manually trim a decision tree
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train,
+             method="class", control=rpart.control(minsplit=2, cp=0.005))
